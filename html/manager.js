696e6089b3849fc8e86604055c5f0cf449e68a96
Haajirah Siddiqi <haajirah.siddiqi@tamu.edu>
2025-11-04 23:27:48 -0600

    fixed dashboard

--- html/manager.js
+++ html/manager.js
@@ -1,13 +1,3 @@
-const sideMenu = document.querySelector("aside");
-const menuBtn = document.querySelector("#menu-btn");
-const closeBtn = document.querySelector("#close-btn");
-
-menuBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'block';
-});
-closeBtn.addEventListener('click', () =>{
-    sideMenu.style.display ='none';
-});
 
 // html/manager.js
 document.addEventListener("DOMContentLoaded", () => {
