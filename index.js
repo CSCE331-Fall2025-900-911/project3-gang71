c403f5c8256a94edcf3a8650e1291b484e31b3bf
csclaire <122688652+csclaire@users.noreply.github.com>
2025-11-05 21:47:46 -0600

    Pulled drinks dynamically, added temporary modifications popup, used environment variables to connect to db

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.use(express.static(path.join(__dirname, "html"))); // Serve /html folder
 
 // PostgreSQL connection
 const pool = new Pool({
-  user: "gang_71",
-  host: "csce-315-db.engr.tamu.edu",
-  database: "gang_71_db",
-  password: "nxzB4p7Y",
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   port: 5432,
   ssl: { rejectUnauthorized: false }, // needed for secure remote connections
 });
@@ -40,6 +40,23 @@ app.get("/api/inventory", async (req, res) => {
     res.json(result.rows);
   } catch (err) {
     console.error("Database error:", err);
+    res.status(500).json({ error: "Database query for inventory failed" });
+  }
+});
+
+
+app.get("/api/menu/:category", async (req, res) => {
+  try {
+    const category = req.params.category;
+    console.log("Category requested:", category); // <--- check this
+    const result = await pool.query(
+      "SELECT * FROM menu WHERE itemcategory = $1",
+      [category]
+    );
+    console.log("Rows returned:", result.rows.length); // <--- check this
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
     res.status(500).json({ error: "Database query failed" });
   }
 });
